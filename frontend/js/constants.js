3ca822c522c05b85d63f361fc69294fc47ec3cc8
David Bradford <59341428+DSBDesignAgency@users.noreply.github.com>
2022-03-22 14:37:43 -0400

    contract updates

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xCD44352Ba2Be497bDE30Af36331FD0001943F0C4"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to The Crater Faces NFT Project!";
